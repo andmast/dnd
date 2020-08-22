8e567933c6ea40f800252708e86d02951db68c35
Andrea <anmastrantoni@gmail.com>
2020-08-22 18:09:03 -0400

    Toggle and button for monster details

--- src/Monsters/MonsterCard.js
+++ src/Monsters/MonsterCard.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const MonsterCard = ({ name }) => {
+export const MonsterCard = ({ name, url }) => {
+  const [showDetails, setShowDetails] = useState(false);
   return (
     <div>
       <p>{name}</p>
+      <button onClick={() => setShowDetails(!showDetails)}>
+        monsterDetails
+      </button>
+      {showDetails && <div>monsterDetails, {url}</div>}
     </div>
   );
 };
